b4f850231bcae342c36bb9e1b5f78eff91f2e1d3
Hexirp <Hexirp@users.noreply.github.com>
2019-02-08 21:14:54 +0900

    Update: 外延性の公理を定義
    ついでに class から object と trait へ変更。定義しようとしたら気が
    付いた。

--- src/main/scala/io/github/hexirp/scaqula/ZFC.scala
+++ src/main/scala/io/github/hexirp/scaqula/ZFC.scala
@@ -6,14 +6,19 @@ package io.github.hexirp.scaqula
   * 代わりに forall A B : Type, A <\: Set -> B <\: Set -> Type という風に部分型関係で表現する。
   *
   */
-class ZFC {
+object ZFC {
 
-  class ZfcModel {
+  trait ZfcModel {
 
     type Set
     type Elem[_ <: Set, _ <: Set]
 
-    def extension[A <: Set, B <: Set]() : Unit = ()
+    trait ExtensionalEquality[A <: Set, B <: Set] {
+      def left[X <: Set] : Elem[X, A] => Elem[X, B]
+      def right[X <: Set] : Elem[X, B] => Elem[X, A]
+    }
+
+    def extension[A <: Set, B <: Set] : ExtensionalEquality[A, B] => A =:= B
 
   }
 
