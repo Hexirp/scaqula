4ebe88018c50f2310b4d416bf0f71a2520f082af
Hexirp <Hexirp@users.noreply.github.com>
2019-02-08 21:01:28 +0900

    Update: 部分型関係カインドを再現すると方針を定める

--- src/main/scala/io/github/hexirp/scaqula/ZFC.scala
+++ src/main/scala/io/github/hexirp/scaqula/ZFC.scala
@@ -1,11 +1,17 @@
 package io.github.hexirp.scaqula
 
+/** [[ZFC]] は、ZFC 集合論の Scala での表現を行う。
+  *
+  * Set は集合の型である。Elem は Set -> Set -> Type というカインドを持たせたかったが、出来ないので
+  * 代わりに forall A B : Type, A <\: Set -> B <\: Set -> Type という風に部分型関係で表現する。
+  *
+  */
 class ZFC {
 
   class ZfcModel {
 
     type Set
-    type Elem[_, _]
+    type Elem[_ <: Set, _ <: Set]
 
   }
 
